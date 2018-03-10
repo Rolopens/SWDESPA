91e808d6bbef27fa9924411dee01291bca2c49e1
Miggy <antonio_llamas@dlsu.edu.ph>
2018-03-11 03:09:35 +0800

    Adding activity table etc

--- DesignChallange2/src/view/CalendarProgram.java
+++ DesignChallange2/src/view/CalendarProgram.java
@@ -1,12 +1,7 @@
 package view;
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
- * @author Arturo III
+ * @author Arturo III; Llamas, Antonio Miguel B.; Pena, Romeo Manuel N.
  */
 import controller.CalendarController;
 import javax.swing.*;
@@ -18,14 +13,13 @@ import java.util.*;
 import model.EventsObject;
 
 public class CalendarProgram {
-
     /**
-     * ** Day Components ***
+     * ** Day components ***
      */
     public int yearBound, monthBound, dayBound, yearToday, monthToday;
 
     /**
-     * ** Swing Components ***
+     * ** Swing components ***
      */
     public JLabel monthLabel, yearLabel;
     public JButton btnPrev, btnNext;
@@ -45,12 +39,21 @@ public class CalendarProgram {
     public JRadioButton taskButton;
 
     /**
-     * ** Calendar Table Components **
+     * ** Calendar table components **
      */
     public JTable calendarTable;
     public DefaultTableModel modelCalendarTable;
+    
+    /**
+     * ** Activity table components **
+     */
+    public JScrollPane scrollActivityTable;
+    public JTable activityTable;
+    public DefaultTableModel modelActivityTable;
+    public JFrame activityFrame;
+    public JPanel activityPanel;
 
-    //List of events
+    // List of events
     private ArrayList<EventsObject> events = new ArrayList<EventsObject>();
     private CalendarController controller;
 
@@ -62,7 +65,6 @@ public class CalendarProgram {
         this.controller = controller;
     }
 
-    
     public void setEventsArrayList(ArrayList<EventsObject> events) {
         this.events = events;
     }
@@ -102,31 +104,27 @@ public class CalendarProgram {
                 
         String[] timeslots = new String[]{"0:00", "0:30", "1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", 
                                             "5:00", "5:30", "6:00","6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", 
-                                            "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30","14:00","14:30",
-                                            "15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30","20:00",
-                                            "20:30","21:00","21:30", "22:00","22:30","23:00","23:30",};
+                                            "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30",
+                                            "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00",
+                                            "20:30", "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"};
         
         startTime = new JComboBox();
         endTime = new JComboBox();
         endTime.setModel(new DefaultComboBoxModel(timeslots));
         startTime.setModel(new DefaultComboBoxModel(timeslots));
-        start = new JLabel("Start Time");
-        end = new JLabel("End Time");
+        start = new JLabel("Start time");
+        end = new JLabel("End time");
         eventButton = new JRadioButton("Event");
         taskButton = new JRadioButton("Task");
         dateArea = new JTextArea();
-        eventName = new JTextArea("Enter Name of Event/Task");
+        eventName = new JTextArea("Enter name of event/task");
         addEvent = new JButton("Add");
-        
-        
         
         btnPrev = new JButton("<<");
         btnNext = new JButton(">>");
         
         dateArea.setEditable(false);
-        
-        
-        
+          
         modelCalendarTable = new DefaultTableModel() {
             @Override
             public boolean isCellEditable(int rowIndex, int mColIndex) {
@@ -144,8 +142,7 @@ public class CalendarProgram {
         calendarTable.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent evt) {
-                
-                try{
+                try {
                     int col = calendarTable.getSelectedColumn();  
                     int row = calendarTable.getSelectedRow();
                     dateArea.setText(yearToday + "-" + (monthToday+1) +"-"+ modelCalendarTable.getValueAt(row, col).toString().split(" ")[0]);
@@ -236,7 +233,64 @@ public class CalendarProgram {
         for (int i = yearBound - 100; i <= yearBound + 100; i++) {
             cmbYear.addItem(String.valueOf(i));
         }
+        
+        String[] activityHeaders = {"Time", "Activity"};
+        modelActivityTable = new DefaultTableModel() {
+            @Override
+            public String getColumnName(int index) {
+                return activityHeaders[index];
+            }
+        };
+        
+        modelActivityTable.setColumnCount(2);
+        modelActivityTable.setRowCount(48);
+        
+        for(int i = 0; i < 48; i++) {
+            modelActivityTable.setValueAt(timeslots[i], i, 0);
+        }
+        
+        activityTable = new JTable(modelActivityTable) {
+            @Override
+            public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+                JComponent component = (JComponent) super.prepareRenderer(renderer, row, column);
+                if(getValueAt(row, 0).toString().contains(":30") && column == 0)
+                    component.setForeground(Color.WHITE);
+                else
+                    component.setForeground(Color.BLACK);
+                return component;
+            }
+            @Override
+            public boolean isCellEditable(int rowIndex, int mColIndex) {
+                return false;
+            }
+        };
+        // activityTable.setBounds(660, 20, 600, 650);
+        activityPanel = new JPanel();
+        activityPanel.setBounds(660, 20, 600, 650);
+        activityPanel.setVisible(true);
+        // activityFrame.add(activityTable);
+        scrollActivityTable = new JScrollPane(activityTable);
+        activityPanel.add(scrollActivityTable);
+        pane.add(activityPanel);
+        // scrollActivityTable = new JScrollPane(activityTable);
+        /*
+        activityPanel = new JPanel(null);
+        pane.add(activityPanel);
+        activityPanel.setBounds(0, 0, 1000, 780);
+        // activityTable.getParent().setBackground(activityTable.getBackground()); //Set background
 
+        activityTable.getTableHeader().setResizingAllowed(false);
+        activityTable.getTableHeader().setReorderingAllowed(false);
+
+        activityTable.setColumnSelectionAllowed(true);
+        activityTable.setRowSelectionAllowed(true);
+        activityTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        activityTable.setRowHeight(56);
+        modelActivityTable.setColumnCount(2);
+        modelActivityTable.setRowCount(6);
+        modelActivityTable.setValueAt("Yes", 1, 1);
+        */
         refreshCalendar(monthBound, yearBound); //Refresh calendar
     }
 
